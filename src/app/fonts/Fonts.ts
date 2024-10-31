9278da455b71028ee1c14268c45da510fbdbe0e2
zahirw <zahir.wakid@nusameta.io>
2024-11-01 04:17:49 +0700

    add name bride and groom; add surrah section

--- src/app/fonts/Fonts.ts
+++ src/app/fonts/Fonts.ts
@@ -2,5 +2,7 @@
 import localFont from "next/font/local";
 
 const DearMother = localFont({ src: "DearMother.ttf" });
+const LTRemark = localFont({ src: "LTRemark.otf" });
+const Cinzel = localFont({ src: "Cinzel.ttf" });
 
-export { DearMother };
+export { DearMother, LTRemark, Cinzel };
